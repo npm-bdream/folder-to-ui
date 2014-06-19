68ccd86aa21d39dd39b7fad71b414f456f209c4e
eleridoc <eleridoc@gmail.com>
2014-06-20 00:57:38 +0200

    Add bases for multi-theme and refactor simple folder result with table and actions

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,8 @@
 var folderToUiAppControllers = angular.module('folderToUiAppControllers', []);
 
 folderToUiAppControllers.controller('MainCtrl', ['$scope','$location', function($scope,$location){
+    $scope.conf_ui_theme = 'default';
+
     $scope.changeView = function(view){
         $location.path(view); // path not hash
     }
