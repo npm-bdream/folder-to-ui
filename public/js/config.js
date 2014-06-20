1979a3c445731cf551929082b521f7eed7a974c2
bdream <npm.bdream@gmail.com>
2014-06-20 18:03:20 +0200

    manage file type just start config file for now

---
+++ public/js/config.js
@@ -0,0 +1,67 @@
+var config = {};
+
+config.managedType = {
+
+    "archive":"fa-file-archive-o",
+    "sound":"fa-music",
+    "video":"fa-film",
+    "code":"fa-file-code-o",
+    "text":"fa-file-text-o",
+    "image":"fa-file-image-o",
+    "script":"fa-terminal"
+};
+
+config.extension = {
+
+    "mp3":config.managedType.sound,
+    "flac":config.managedType.sound,
+    "alac":config.managedType.sound,
+    "wav":config.managedType.sound,
+    "wma":config.managedType.sound,
+    "mid":config.managedType.sound,
+    "mpa":config.managedType.sound,
+    "m3u":config.managedType.sound,
+    "ogg":config.managedType.sound,
+
+
+    "avi":config.managedType.video,
+    "m4v":config.managedType.video,
+    "flv":config.managedType.video,
+    "mov":config.managedType.video,
+    "mp4":config.managedType.video,
+    "mpg":config.managedType.video,
+    "vob":config.managedType.video,
+    "wmv":config.managedType.video,
+    "mpeg":config.managedType.video,
+    "divx":config.managedType.video,
+    "mkv":config.managedType.video,
+    "ogv":config.managedType.video,
+    "xvid":config.managedType.video,
+
+    "7z":config.managedType.archive,
+    "cbr":config.managedType.archive,
+    "deb":config.managedType.archive,
+    "gz":config.managedType.archive,
+    "pkg":config.managedType.archive,
+    "rar":config.managedType.archive,
+    "rpm":config.managedType.archive,
+    "tar.gz":config.managedType.archive,
+    "zip":config.managedType.archive,
+    "zipx":config.managedType.archive,
+    "tgz":config.managedType.archive,
+
+    "asp":config.managedType.code,
+    "aspx":config.managedType.code,
+    "css":config.managedType.code,
+    "htm":config.managedType.code,
+    "html":config.managedType.code,
+    "js":config.managedType.code,
+    "jsp":config.managedType.code,
+    "php":config.managedType.code,
+    "rss":config.managedType.code,
+    "xhtml":config.managedType.code,
+    "htaccess":config.managedType.code,
+    "xml":config.managedType.code,
+    "json":config.managedType.code,
+    "wml":config.managedType.code
+};
