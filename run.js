de1305c804a24a20b905171eedd5398945b4279a
eleridoc <eleridoc@gmail.com>
2014-06-22 21:02:51 +0200

    working for database just try ...

--- run.js
+++ run.js
@@ -1,31 +1,74 @@
 var Express = require('express'),
     Morgan = require('morgan'),
-    BodyParser = require('body-parser')
+    BodyParser = require('body-parser'),
     FolderContents = require('folder-contents'),
     Util = require('util'),
     Colors = require('colors'),
-    Config = require('./server/config.js');
-
-
+    Config = require('./server/config.js'),
+    Fs = require("fs"),
+    Sqlite3 = require("sqlite3").verbose();
 
 Colors.setTheme({
-    silly: 'rainbow',
-    input: 'grey',
-    verbose: 'cyan',
-    prompt: 'grey',
     info: 'green',
-    data: 'grey',
-    help: 'cyan',
-    warn: 'yellow',
-    debug: 'blue',
-    error: 'red'
+    verbose: 'cyan',
+    service: 'magenta',
+    error: 'red',
+    debug: 'blue'
 });
+
+
+var database = __dirname + "/test.db";
+var databaseExists = Fs.existsSync(database);
+
+if(!databaseExists) {
+    console.log("Creating DB file.");
+    Fs.openSync(database, "w");
+}
+
+var db = new Sqlite3.Database(database);
+
+db.serialize(function() {
+    if(!databaseExists) {
+        db.run("CREATE TABLE Users (login TEXT,password TEXT,email TEXT )");
+    }
+
+    var stmt = db.prepare("INSERT INTO Users VALUES (?,?,?)");
+
+//Insert random data
+    var rnd;
+    for (var i = 0; i < 10; i++) {
+        rnd = Math.floor(Math.random() * 10000000);
+        stmt.run(["login #" + rnd,"password #" + rnd,"email #" + rnd]);
+    }
+
+    stmt.finalize();
+    db.each("SELECT rowid AS id, password FROM Users", function(err, row) {
+        console.log(row.id + ": " + row.password);
+    });
+});
+
+//db.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var app = Express();
 
 app.use(Morgan());
-
-app.set('title', 'folder-to-ui');
 
 if (Config.server_public_base == "") Config.server_public_base = __dirname;
 if (Config.server_sharing_base == "") Config.server_sharing_base = __dirname;
@@ -33,14 +76,29 @@ if (Config.server_sharing_base == "") Config.server_sharing_base = __dirname;
 // simple logger
 app.all(Config.server_sharing_ui_path+'/*',function(req, res, next){
     //res.send("404","Test restricted access");
-    res.redirect('/403.html');
+    var testToreturn = "toto";
+    db.serialize(function() {
+
+
+
+        db.all("SELECT rowid AS id, password FROM Users", function(err, rows) {
+            console.log(rows);
+            res.send(rows);
+        });
+
+
+    });
+
+
+
+    //res.redirect('/403.html');
     //next();
 });
 
-Util.log(("Public web folder : " + Config.server_public_base + Config.server_public_dir).help);
+Util.log(("Public web folder : " + Config.server_public_base + Config.server_public_dir).bold.verbose);
 app.use(Express.static( Config.server_public_base + Config.server_public_dir ));
 
-Util.log(("Sharing web folder : " + Config.server_sharing_base + Config.server_sharing_dir).help);
+Util.log(("Sharing web folder : " + Config.server_sharing_base + Config.server_sharing_dir).bold.verbose);
 app.use(Config.server_sharing_ui_path, Express.static( Config.server_sharing_base + Config.server_sharing_dir ));
 
 app.use(BodyParser.json());       // to support JSON-encoded bodies
@@ -50,10 +108,6 @@ app.use(BodyParser.urlencoded()); // to support URL-encoded bodies
 
 app.post('/api/folder/list', function(req, res){
 
-    /*
-    Util.log("Request body : ".help);
-    */
-
     var options = req.body;
 
     // force usage of basepath to false
@@ -62,14 +116,14 @@ app.post('/api/folder/list', function(req, res){
     // force to use server base path
     options.path = Config.server_sharing_base + Config.server_sharing_dir + options.path;
 
-    Util.log(("Requested folder : " + options.path).debug);
+    Util.log(("Requested folder : " + options.path).service);
 
     var jsonResult = FolderContents(options);
 
-    /*
-    Util.log("Result : ".help);
-    Util.log(JSON.stringify(jsonResult));
-    */
+
+    Util.log("Result : ".service);
+    Util.log((JSON.stringify(jsonResult)).service);
+
     res.send(JSON.stringify(jsonResult));
 
 });
