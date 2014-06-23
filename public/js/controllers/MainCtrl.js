b9324e24d49aec1bbc996a5d3a1f320b6274c331
bdream <npm.bdream@gmail.com>
2014-06-23 18:01:41 +0200

    In work

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,5 @@
 folderToUiAppControllers.controller('MainCtrl', ['$scope','$location', function($scope,$location){
     $scope.conf_ui_theme = 'default';
-
     $scope.changeView = function(view){
         $location.path(view); // path not hash
     }
--- run.js
+++ run.js
@@ -6,7 +6,10 @@ var Express = require('express'),
     Colors = require('colors'),
     Config = require('./server/config.js'),
     Fs = require("fs"),
-    Sqlite3 = require("sqlite3").verbose();
+    Sqlite3 = require("sqlite3").verbose(),
+    jsSHA = require("jssha"),
+    CookieParser = require('cookie-parser'),
+    Session = require('express-session');
 
 Colors.setTheme({
     info: 'green',
@@ -21,89 +24,63 @@ var database = __dirname + "/test.db";
 var databaseExists = Fs.existsSync(database);
 
 if(!databaseExists) {
-    console.log("Creating DB file.");
+    Util.log("Creating DB file.".bold.info);
     Fs.openSync(database, "w");
 }
 
 var db = new Sqlite3.Database(database);
-
 db.serialize(function() {
     if(!databaseExists) {
-        db.run("CREATE TABLE Users (login TEXT,password TEXT,email TEXT )");
+        db.run("CREATE TABLE users (login TEXT,password TEXT,email TEXT )");
+        db.run("CREATE TABLE server_allow_ip (ip TEXT UNIQUE)");
+        db.run("CREATE TABLE server_ignore_extension (ext TEXT UNIQUE)");
+        db.run("CREATE TABLE server_ignore_file (file TEXT UNIQUE)");
+        db.run("CREATE TABLE server_ignore_folder (folder TEXT UNIQUE)");
+
+        var shaObj = new jsSHA("admin", "TEXT");
+        var hash = shaObj.getHash("SHA-512", "HEX");
+
+        var stmt = db.prepare("INSERT INTO users VALUES (?,?,?)");
+        stmt.run(["admin",hash,"npm.dream@gmail.com"]);
+        stmt.finalize();
     }
-
-    var stmt = db.prepare("INSERT INTO Users VALUES (?,?,?)");
-
-//Insert random data
-    var rnd;
-    for (var i = 0; i < 10; i++) {
-        rnd = Math.floor(Math.random() * 10000000);
-        stmt.run(["login #" + rnd,"password #" + rnd,"email #" + rnd]);
-    }
-
-    stmt.finalize();
-    db.each("SELECT rowid AS id, password FROM Users", function(err, row) {
-        console.log(row.id + ": " + row.password);
+    db.each("SELECT rowid AS id, password, login, email FROM users", function(err, row) {
+        Util.log(row.id + ": " + row.password);
     });
 });
-
-//db.close();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var app = Express();
-
-app.use(Morgan());
+db.close();
 
 if (Config.server_public_base == "") Config.server_public_base = __dirname;
 if (Config.server_sharing_base == "") Config.server_sharing_base = __dirname;
 
-// simple logger
+var app = Express();
+
+app.use(Morgan());
+app.use(BodyParser.json());       // to support JSON-encoded bodies
+app.use(BodyParser.urlencoded()); // to support URL-encoded bodies
+app.use(Session({ secret: 'My great secret', cookie: { maxAge: 60000 }}));
+app.use(CookieParser());
+
 app.all(Config.server_sharing_ui_path+'/*',function(req, res, next){
     //res.send("404","Test restricted access");
     var testToreturn = "toto";
     db.serialize(function() {
-
-
 
         db.all("SELECT rowid AS id, password FROM Users", function(err, rows) {
             console.log(rows);
             res.send(rows);
         });
 
-
     });
-
-
-
     //res.redirect('/403.html');
     //next();
 });
 
-Util.log(("Public web folder : " + Config.server_public_base + Config.server_public_dir).bold.verbose);
+Util.log(("Public web folder : " + Config.server_public_base + Config.server_public_dir).bold.info);
 app.use(Express.static( Config.server_public_base + Config.server_public_dir ));
 
-Util.log(("Sharing web folder : " + Config.server_sharing_base + Config.server_sharing_dir).bold.verbose);
+Util.log(("Sharing web folder : " + Config.server_sharing_base + Config.server_sharing_dir).bold.info);
 app.use(Config.server_sharing_ui_path, Express.static( Config.server_sharing_base + Config.server_sharing_dir ));
-
-app.use(BodyParser.json());       // to support JSON-encoded bodies
-app.use(BodyParser.urlencoded()); // to support URL-encoded bodies
-
 
 
 app.post('/api/folder/list', function(req, res){
